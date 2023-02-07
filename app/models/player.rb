37e014c21fdb9db81f0e50d56a0470b0eb4466e3
tim <litidm@gmail.com>
2023-02-07 03:09:50 +0300

    add reached performance method for player

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,4 +3,11 @@ class Player < ApplicationRecord
   belongs_to :team
 
   validates :first_name, :last_name, presence: true
+
+  def reached_performance?(performance_goal_title, matches_count)
+    performance_goal_id = PerformanceGoal.find_by(title: performance_goal_title.to_sym).id
+    PlayerPerformance.where(performance_goal_id: performance_goal_id, player_id: self.id)
+                     .last(matches_count)
+                     .inject(false) { |result, player_performance| result || player_performance.reached_performance }
+  end
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -10,4 +10,34 @@ RSpec.describe Player, type: :model do
     it { should validate_presence_of :first_name }
     it { should validate_presence_of :last_name }
   end
+
+  describe '#reached_performance?' do
+    let(:teams) { create_list(:team, 2) }
+    let(:player) { create(:player, team_id: teams.first.id) }
+    let(:ended_match) { create(:match, home_team_id: teams.first.id, away_team_id: teams.last.id) }
+    let(:performance_goal) { create(:performance_goal) }
+    let!(:not_reached_performance_goals) do
+      create_list(:player_performance,
+                  4,
+                  match_id: ended_match.id,
+                  performance_goal_id: performance_goal.id,
+                  player_id: player.id)
+    end
+    let!(:reached_performance_goal) do
+      create(:player_performance,
+             match_id: ended_match.id,
+             performance_goal_id: performance_goal.id,
+             player_id: player.id,
+             reached_performance: true)
+    end
+
+    it 'returns true if performance goal reached at least 1 time in 5 matches' do
+      expect(player.reached_performance?(performance_goal.title, 5)).to be_truthy
+    end
+
+    it 'returns false if performance goal not reached at least 1 time in 5 matches' do
+      reached_performance_goal.update(reached_performance: false)
+      expect(player.reached_performance?(performance_goal.title, 5)).to be_falsey
+    end
+  end
 end
